15d8d4d5238d29bbfcb443cd5de7575999febc4d
guoqingping <1248019684@qq.com>
2024-06-22 16:03:27 +0800

    test

--- log4go_test.go
+++ log4go_test.go
@@ -1,6 +1,7 @@
 package log4go
 
 import (
+	"context"
 	"github.com/sirupsen/logrus"
 	"log4go/writer"
 	"testing"
@@ -12,4 +13,14 @@ func TestLog4GoInit(t *testing.T) {
 	logrus.Warn("******* test log4go init2 ******")
 	logrus.Debug("******* test log4go init3 ******")
 	logrus.Error("******* test log4go init4 ******")
+}
+
+func TestLog4Go(t *testing.T) {
+	Log4GoInit("./log", writer.File_StdOut, writer.DebugLevel)
+
+	ctx := context.WithValue(context.Background(), "trace_id", "12345678")
+	Info(ctx, "test info")
+
+	log := NewLog4Go("daluandou_main")
+	log.Infof(ctx, "test info")
 }
