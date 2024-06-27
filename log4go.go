451d707f6a23def9c59943b9d3fb73e2c08780d0
guoqingping <1248019684@qq.com>
2024-06-27 10:52:44 +0800

    add InitWithProfile

--- log4go.go
+++ log4go.go
@@ -10,6 +10,12 @@ import (
 	"os"
 )
 
+/**
+ * 日志组件初始化
+ * logFilePath 日志文件路径
+ * writeType 输出类型(日志文件 标准输出 文件和标准输出）
+ * level 日志级别
+ */
 func Log4GoInit(logFilePath string, writeType writer.WriterType, level writer.LogLevel) {
 	rotatelogWriter, errWriter := writer.GetFileWriter(logFilePath)
 	logWriter := writer.GetLogWriter(rotatelogWriter, writeType)
@@ -51,4 +57,23 @@ func getLogRusLever(level writer.LogLevel) logrus.Level {
 	default:
 		return logrus.InfoLevel
 	}
+}
+
+/**
+ * 根据运行环境初始化日志组件
+ * profile 运行环境 dev开发环境，debug,输出为(标准输出)控制台+文件 test为测试环境，debug,输出到文件
+ * logFilePath 日志文件路径，默认路径为/data/logs/
+ */
+func InitWithProfile(profile string, logFilePath string) {
+	var logLevel writer.LogLevel = writer.InfoLevel
+	var writeType writer.WriterType = writer.File_Write
+	if profile == "dev" {
+		logLevel = writer.DebugLevel
+		writeType = writer.File_StdOut
+	}
+	if profile == "test" {
+		logLevel = writer.DebugLevel
+		writeType = writer.File_Write
+	}
+	Log4GoInit(logFilePath, writeType, logLevel)
 }
